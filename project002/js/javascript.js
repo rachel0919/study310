c9ea3e9eb2df4bdcbd1604c8fce781646c8b4b20
rachel0919 <chaiheran@gmail.com>
2025-02-24 11:49:06 +0900

    20250224

--- project002/js/javascript.js
+++ project002/js/javascript.js
@@ -2,12 +2,23 @@
 
 $(document).ready(function(){
 
-
-    // 실행문
-
-    
+    window.addEventListener('scroll', function() {
+        const header = document.querySelector('.header');
+        const heroSection = document.querySelector('.hero');
+        const heroHeight = heroSection.offsetHeight;
+      
+        // Hero 섹션을 지나면 배경색을 추가
+        if (window.scrollY > heroHeight) {
+          header.classList.add('scrolled');
+        } else {
+          header.classList.remove('scrolled');
+        }
+      });
 
 
 
 
 })     //제이쿼리 끝!!!!!!!!
+
+
+
